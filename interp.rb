52ffde1b313c83745d0595c60d58477536b5c83c
Sangyong Sim <sangyong-sim@cookpad.com>
2018-10-05 14:28:47 +0900

    Self host

--- interp.rb
+++ interp.rb
@@ -17,63 +17,73 @@ def fizzbuzz(n)
 end
 
 # An implementation of the evaluator
-def evaluate(exp, env)
+def evaluate(exp, lenv, function_definitions)
   # exp: A current node of AST
-  # env: An environment (explained later)
+  # lenv: An environment (explained later)
 
   case exp[0]
   when "lit"
     exp[1] # return the immediate value as is
   when "+"
-    evaluate(exp[1], env) + evaluate(exp[2], env)
+    evaluate(exp[1], lenv, function_definitions) + evaluate(exp[2], lenv, function_definitions)
   when "-"
-    evaluate(exp[1], env) - evaluate(exp[2], env)
+    evaluate(exp[1], lenv, function_definitions) - evaluate(exp[2], lenv, function_definitions)
   when "*"
-    evaluate(exp[1], env) * evaluate(exp[2], env)
+    evaluate(exp[1], lenv, function_definitions) * evaluate(exp[2], lenv, function_definitions)
   when "%"
-    evaluate(exp[1], env) % evaluate(exp[2], env)
+    evaluate(exp[1], lenv, function_definitions) % evaluate(exp[2], lenv, function_definitions)
   when "/"
-    evaluate(exp[1], env) / evaluate(exp[2], env)
+    evaluate(exp[1], lenv, function_definitions) / evaluate(exp[2], lenv, function_definitions)
   when "=="
-    evaluate(exp[1], env) == evaluate(exp[2], env)
+    evaluate(exp[1], lenv, function_definitions) == evaluate(exp[2], lenv, function_definitions)
   when ">"
-    evaluate(exp[1], env) > evaluate(exp[2], env)
+    evaluate(exp[1], lenv, function_definitions) > evaluate(exp[2], lenv, function_definitions)
   when "<"
-    evaluate(exp[1], env) < evaluate(exp[2], env)
+    evaluate(exp[1], lenv, function_definitions) < evaluate(exp[2], lenv, function_definitions)
   when "stmts"
     i = 1
     last = nil
     while exp[i]
-      list = evaluate(exp[i], env)
-      i += 1
+      last = evaluate(exp[i], lenv, function_definitions)
+      i = i + 1
     end
     last
+  when "const_ref"
+    eval(exp[1])
   when "var_ref"
-    env[exp[1]]
+    lenv[exp[1]]
   when "var_assign"
-    env[exp[1]] = evaluate(exp[2], env)
+    lenv[exp[1]] = evaluate(exp[2], lenv, function_definitions)
   when "if"
-    if evaluate(exp[1], env)
-      evaluate(exp[2], env)
+    if evaluate(exp[1], lenv, function_definitions)
+      evaluate(exp[2], lenv, function_definitions)
     else
-      evaluate(exp[3], env)
+      evaluate(exp[3], lenv, function_definitions)
     end
   when "while"
-    while evaluate(exp[1], env)
-      evaluate(exp[2], env)
+    while evaluate(exp[1], lenv, function_definitions)
+      evaluate(exp[2], lenv, function_definitions)
     end
   when "func_call"
     # Lookup the function definition by the given function name.
-    func = $function_definitions[exp[1]]
+    func = function_definitions[exp[1]]
 
-    if func.nil?
+    # builtin
+    if func == nil
       case exp[1]
       when "p"
-        p(evaluate(exp[2], env))
+        p(evaluate(exp[2], lenv, function_definitions))
+      when "pp"
+        pp(evaluate(exp[2], lenv, function_definitions))
       when "Integer"
-        Integer(evaluate(exp[2], env))
+        Integer(evaluate(exp[2], lenv, function_definitions))
       when "fizzbuzz"
-        fizzbuzz(evaluate(exp[2], env))
+        fizzbuzz(evaluate(exp[2], lenv, function_definitions)) when "require"
+        require(evaluate(exp[2], lenv, function_definitions))
+      when "minruby_parse"
+        minruby_parse(evaluate(exp[2], lenv, function_definitions))
+      when "minruby_load"
+        minruby_load()
       else
         puts exp[1]
         raise("unknown builtin function")
@@ -83,7 +93,7 @@ def evaluate(exp, env)
       args = []
       i = 0
       while exp[i + 2]
-        args[i] = evaluate(exp[i + 2], env)
+        args[i] = evaluate(exp[i + 2], lenv, function_definitions)
         i = i + 1
       end
       # prepare param
@@ -95,34 +105,33 @@ def evaluate(exp, env)
         i = i + 1
       end
       # eval
-      evaluate(func[1], new_env)
+      evaluate(func[1], new_env, function_definitions)
     end
-
   when "func_def"
-    $function_definitions[exp[1]] = [exp[2], exp[3]]
+    function_definitions[exp[1]] = [exp[2], exp[3]]
   when "ary_new"
     ary = []
     i = 0
     while exp[i + 1]
-      ary[i] = evaluate(exp[i + 1], env)
+      ary[i] = evaluate(exp[i + 1], lenv, function_definitions)
       i = i + 1
     end
     ary
   when "ary_ref"
-    ary = evaluate(exp[1], env)
-    idx = evaluate(exp[2], env)
+    ary = evaluate(exp[1], lenv, function_definitions)
+    idx = evaluate(exp[2], lenv, function_definitions)
     ary[idx]
   when "ary_assign"
-    ary = evaluate(exp[1], env)
-    idx = evaluate(exp[2], env)
-    val = evaluate(exp[3], env)
+    ary = evaluate(exp[1], lenv, function_definitions)
+    idx = evaluate(exp[2], lenv, function_definitions)
+    val = evaluate(exp[3], lenv, function_definitions)
     ary[idx] = val
   when "hash_new"
     hsh = {}
     i = 0
     while exp[i + 1]
-      key = evaluate(exp[i + 1], env)
-      val = evaluate(exp[i + 2], env)
+      key = evaluate(exp[i + 1], lenv, function_definitions)
+      val = evaluate(exp[i + 2], lenv, function_definitions)
       hsh[key] = val
       i = i + 2
     end
@@ -134,9 +143,9 @@ def evaluate(exp, env)
   end
 end
 
-$function_definitions = {}
-env = {}
+function_definitions = {}
+lenv = {}
 
 # `minruby_load()` == `File.read(ARGV.shift)`
 # `minruby_parse(str)` parses a program text given, and returns its AST
-evaluate(minruby_parse(minruby_load()), env)
+evaluate(minruby_parse(minruby_load()), lenv, function_definitions)
